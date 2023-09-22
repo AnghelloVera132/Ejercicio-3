2ceb359232a2b9f5e98700b8a63bacc46404feed
User <User@DESKTOP-411OTHH>
2023-09-21 21:46:25 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el número de notas: ");
+            int numero = int.Parse(Console.ReadLine());
+            float suma = 0f;
+            for (int i = 0; i < numero; i++)
+            {
+                Console.Write($"Ingrese la nota{i}: ");
+                float notas = float.Parse(Console.ReadLine());
+                suma += notas;
+            }
+            float resoult;
+            resoult = suma / numero;
+            Console.Write($"El promedio es: {resoult}");
+            Console.ReadKey();
+        }
+    }
+}
